37adff3379a09e70184343e747d06879c0e7586c
Simon Wilper (epsilon) <sxw@chronowerks.de>
2015-01-30 12:30:32 +0100

    version bump

--- inc/main.hpp
+++ inc/main.hpp
@@ -28,7 +28,7 @@
 #include "parser.hpp"
 #include "help.hpp"
 
-#define CONVERT_VERSION "2.1.0"
+#define CONVERT_VERSION "2.1.1"
 #define CONVERT_DATE "2015-01-30"
 
 class Convert : public QMainWindow {
